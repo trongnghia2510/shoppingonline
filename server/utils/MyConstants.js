5f4948159bb4e2e3c4b12fd7c644f2a5bbf894f2
trongnghia2510 <108378520+trongnghia2510@users.noreply.github.com>
2024-07-16 11:03:59 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'clustersgp.gdqhjdv.mongodb.net',
+    DB_USER: 'nghia207ct40473',
+    DB_PASS: 'nghia207ct40473',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'nghia25102002',
+    JWT_EXPIRES: '360000000', // in milliseconds
+    EMAIL_USER: 'olo2108m@gmail.com', // gmail service
+    EMAIL_PASS: 'fisd mybh ypgd htfa'
+  };
+  module.exports = MyConstants;
